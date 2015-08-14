732405f29a6dbd4e5624d278c49ad2d2ef05ca17
Tyler Deem <tdeem6@gmail.com>
2015-08-14 15:27:41 -0700

    Adds working 1st test for finding match in phrase input

--- src/CountRepeats.php
+++ src/CountRepeats.php
@@ -4,6 +4,20 @@
     {
         function RepeatCounter($word, $phrase)
         {
-          
+          //sets variable to 0 for incoming matches
+           $foundMatches = 0;
+          //seperates words in the phrase variable
+           $phrase_array = explode(" ", $phrase);
+
+          //loop for finding matches and storing it in foundMatches variable
+          foreach($phrase_array as $phrase_word)
+          {
+              if ($word == $phrase_word)
+              {
+                $foundMatches = $foundMatches + 1;
+              }
+          }
+
+          return $foundMatches;
         }
     }
